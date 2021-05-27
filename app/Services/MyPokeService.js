7b39164a9c37423d2e3bde5e2fac34eb65267c29
Jonesy <jonesyjava@gmail.com>
2021-05-27 17:40:48 -0600

    wired swal to catch/release

--- app/Services/MyPokeService.js
+++ app/Services/MyPokeService.js
@@ -1,6 +1,8 @@
 import { ProxyState } from "../AppState.js";
 import Pokemon from "../Models/Pokemon.js";
 import { sandboxApi } from "./AxiosService.js";
+import NotificationService from "../Utils/NotificationService.js";
+import CaughtNote from "../Utils/CaughtNote.js";
 
 class MyPokeService{
     constructor(){
@@ -19,10 +21,12 @@ class MyPokeService{
 async catchPokemon(){
     try {
         delete ProxyState.activePokemon._id
+        if(await CaughtNote.confirmNote("GREAT WORK!", "success")){
         let res = await sandboxApi.post("", ProxyState.activePokemon)
         console.log(res)
         ProxyState.myPokemon = [...ProxyState.myPokemon, new Pokemon(res.data)]
         this.getMyPokemon()
+        }
     } catch (error) {
         console.error(error)
     }
@@ -36,9 +40,11 @@ async catchPokemon(){
 
     async releasePokemon(_id){
         try {
-            await sandboxApi.delete(ProxyState.activePokemon._id)
+            if(await NotificationService.confirmNote("Are you sure you want to RELEASE your Pokemon?", "error")){
+            const res = await sandboxApi.delete(ProxyState.activePokemon._id)
             this.getMyPokemon()
             ProxyState.activePokemon = null
+        }
         } catch (error) {
             console.error(error)
         }
--- app/Utils/CaughtNote.js
+++ app/Utils/CaughtNote.js
@@ -1,20 +1,20 @@
-export default class NotificationService {
-    static async confirmNote(title = 'Are you sure you want to DELETE?', icon = "warning") {
+export default class CaughtNote {
+    static async confirmNote(title = 'Are you sure you want to CATCH?', icon = "success") {
         // @ts-ignore
         const res = await Swal.fire({
             position:'top',
             icon: icon,
             title: title,
             confirmButton: '#3085d6',
-            confirmButtonText: 'Release',
+            confirmButtonText: 'Thanks!',
             toast: true,
             showConfirmButton: true,
          })
         if (res.isConfirmed) {
           // @ts-ignore
           Swal.fire(
-            'Released!',
-            'Pokemon has been released back to the wild.',
+            'Got Em!',
+            'Pokemon has been sent to your backpack. Great work!',
             'success'
           )  
           return true
